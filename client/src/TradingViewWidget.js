58567dc76f1ec9564696584f5a5ed9b1cedb20f9
sachin <sachin.vishwakarmas390@gmail.com>
2023-02-17 21:08:51 +0530

    Initial Project SetUp

---
+++ client/src/TradingViewWidget.js
@@ -0,0 +1,79 @@
+// TradingViewWidget.js
+
+import React, { useEffect, useRef } from "react";
+
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget() {
+    const onLoadScriptRef = useRef();
+
+    useEffect(() => {
+        onLoadScriptRef.current = createWidget;
+
+        if (!tvScriptLoadingPromise) {
+        tvScriptLoadingPromise = new Promise((resolve) => {
+            const script = document.createElement("script");
+            script.id = "tradingview-widget-loading-script";
+            script.src = "https://s3.tradingview.com/tv.js";
+            script.type = "text/javascript";
+            script.onload = resolve;
+            document.head.appendChild(script);
+        });
+        }
+
+        tvScriptLoadingPromise.then(
+        () => onLoadScriptRef.current && onLoadScriptRef.current()
+        );
+
+        return () => (onLoadScriptRef.current = null);
+
+        function createWidget() {
+        if (
+            document.getElementById("technical-analysis-chart-demo") &&
+            "TradingView" in window
+        ) {
+            new window.TradingView.widget({
+                container_id: "technical-analysis-chart-demo",
+                width: "100%",
+                height: "100%",
+                autosize: true,
+                symbol: "AAPL",
+                interval: "D",
+                timezone: "exchange",
+                theme: "dark",
+                style: "1",
+                toolbar_bg: "#f1f3f6",
+                withdateranges: true,
+                hide_side_toolbar: false,
+                allow_symbol_change: true,
+                save_image: false,
+                studies: [
+                    "ROC@tv-basicstudies",
+                    "StochasticRSI@tv-basicstudies",
+                    "MASimple@tv-basicstudies",
+                ],
+                show_popup_button: true,
+                popup_width: "100%",
+                popup_height: "100%",
+                locale: "en",
+            });
+        }
+        }
+    }, []);
+
+    return (
+        <div className="tradingview-widget-container">
+            <div id="technical-analysis-chart-demo" ></div>
+            <div className="tradingview-widget-copyright">
+                <a
+                href="https://www.tradingview.com/symbols/AAPL/"
+                rel="noopener"
+                target="_blank"
+                >
+                <span className="blue-text">AAPL stock chart</span>
+                </a>{" "}
+                by TradingView
+            </div>
+        </div>
+    );
+}
